44b59ec8c241b7875017d96cf0c670503f3a8632
Dinarako <dinarakoikell@gmail.com>
2022-03-07 11:25:37 -0800

    dz1

---
+++ lesson1/greeting.js
@@ -0,0 +1,21 @@
+//Prompt and Alert
+const name=prompt("What is your name?");
+alert("Hello "+name);
+
+
+//Array Length comparison
+
+const mommy = [10, 20, 5, 68, 70];
+const daddy = [20,40, 15, 85];
+
+if (mommy.length > daddy.length){
+console.log('mommy taller than daddy');
+}
+else if (mommy.length < daddy.length) {
+console.log('daddy taller than mommy');
+}
+else
+console.log('they are in same height');
+
+
+
---
+++ lesson1/main.js
@@ -0,0 +1,32 @@
+const a = "20"
+const b = 20
+
+let c = "Sanjar"
+
+const obj = {
+    key: "value",//klyuch i znachenie
+    obj2: {
+        dota: 77,
+        kostyl: "Jalil"
+    }
+}
+
+const bool = true
+const bool2 = false
+
+console.log(typeof a)
+console.log(obj.obj2)
+
+const arr = ["Aidana", "Adinai", "Sanjar", "Jalil"]
+console.log(arr[3]);
+
+const first_number = prompt(message: "first number?")
+const second_number = prompt(message: "second number?")
+
+if (first_number > second_number){
+    console.log(first_number + "bigger than" + second_number)
+};
+
+
+
+//const otvet = alert("Homework 10 point")
